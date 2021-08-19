6a738bd29aa51d5d96abaedbd040502150830e5a
Sofia Shnaidman <sshnaidman@provectus.com>
2021-08-19 15:07:20 +0300

    Added lazy loading.

--- odd-platform-ui/src/components/App.tsx
+++ odd-platform-ui/src/components/App.tsx
@@ -2,13 +2,15 @@ import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { DataEntityTypeDictionary } from 'generated-sources';
 import { toolbarHeight } from 'lib/constants';
-import ManagementContainer from './Management/ManagementContainer';
-import DataEntityDetailsContainer from './DataEntityDetails/DataEntityDetailsContainer';
-import OverviewContainer from './Overview/OverviewContainer';
-import SearchContainer from './Search/SearchContainer';
 import AppLoadingSpinnerContainer from './shared/AppLoadingSpinner/AppLoadingSpinnerContainer';
 import AppToolbarContainer from './shared/AppToolbar/AppToolbarContainer';
-import AlertsContainer from './Alerts/AlertsContainer';
+
+// Lazy loading components
+const DataEntityDetailsContainer = React.lazy(() => import('./DataEntityDetails/DataEntityDetailsContainer'));
+const ManagementContainer = React.lazy(() => import('./Management/ManagementContainer'));
+const SearchContainer = React.lazy(() => import('./Search/SearchContainer'));
+const OverviewContainer = React.lazy(() => import('./Overview/OverviewContainer'));
+const AlertsContainer = React.lazy(() => import('./Alerts/AlertsContainer'));
 
 interface AppProps {
   fetchDataEntitiesTypes: () => Promise<DataEntityTypeDictionary>;
@@ -21,27 +23,27 @@ const App: React.FC<AppProps> = ({ fetchDataEntitiesTypes }) => {
   return (
     <div className="App">
       <AppLoadingSpinnerContainer />
-      <Switch>
-        <Route path={['/']} component={AppToolbarContainer} />
-      </Switch>
+      <AppToolbarContainer/>
       <div style={{ paddingTop: `${toolbarHeight}px` }}>
-        <Switch>
-          <Route exact path="/" component={OverviewContainer} />
-          <Route path="/alerts/:viewType?" component={AlertsContainer} />
-          <Route
-            path="/management/:viewType?"
-            component={ManagementContainer}
-          />
-          <Route
-            exact
-            path="/search/:searchId?"
-            component={SearchContainer}
-          />
-          <Route
-            path="/dataentities/:dataEntityId/:viewType?"
-            component={DataEntityDetailsContainer}
-          />
-        </Switch>
+        <React.Suspense fallback={<span>loading..</span>}> {/* TODO: replace with proper content placeholder*/}
+          <Switch>
+            <Route exact path="/" component={OverviewContainer} />
+            <Route path="/alerts/:viewType?" component={AlertsContainer} />
+            <Route
+              path="/management/:viewType?"
+              component={ManagementContainer}
+            />
+            <Route
+              exact
+              path="/search/:searchId?"
+              component={SearchContainer}
+            />
+            <Route
+              path="/dataentities/:dataEntityId/:viewType?"
+              component={DataEntityDetailsContainer}
+            />
+          </Switch>
+        </React.Suspense>
       </div>
     </div>
   );
